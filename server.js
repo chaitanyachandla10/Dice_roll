d8f2208895d5ecfd1850eff3d8a75fcc9827e5e7
chaitanyachandla10 <chaitanyachandla10@gmail.com>
2020-01-24 01:49:51 +0530

    here is yarn and package.json and dice roll game ready

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+myapp = express();
+var expressStaticGzip = require("express-static-gzip");
+var google_crawler = require("google-crawler");
+var path	= require("path");
+var fs = require("fs");
+var https = require("https");
+var http=require('http');
+var nodemailer = require("nodemailer");
+var mongoose = require("mongoose");
+myapp.use(expressStaticGzip
+    (__dirname + '/public',
+        {
+            enableBrotli: true,
+        }
+    )
+);
+myapp.use(express.static(__dirname + '/public'));
+
+myapp.listen(5100,function()
+{
+	console.log('server is running');
+});
+
+
+
